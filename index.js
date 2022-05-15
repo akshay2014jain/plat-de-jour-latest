7c3b7ec9bd6017d4a545efee101ee8ad463edac8
Viraj Shah <virajshah1104@gmail.com>
2022-05-14 20:43:53 -0700

    Its still awesome

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const cors = require('cors');
 var router = express.Router();
 const app = express();
 app.use(cors());
+
 // Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // An api endpoint that returns a short list of items
 app.get('/getData', (req,res) => {
@@ -16,6 +18,10 @@ app.get('/getData', (req,res) => {
     console.log('Sent list of items');
 });
 
+// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    });
 
 const port = process.env.PORT || 5000;
 app.listen(port);
