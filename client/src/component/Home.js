492b86c727cd6e90bf7e8a9c3934b7d7abe27086
Viraj Shah <virajshah1104@gmail.com>
2022-05-26 13:41:59 -0700

    Changes galore

---
+++ client/src/component/Home.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import styled from "styled-components";
+import './home.css'
+import Card  from './Card';
+import Header from './Header';
+import Feature from './feature';
+import Footer from './footer';
+import Result from './Result';
+
+const Button = styled.button`
+background-color: #FF4820;
+color: white;
+font-size: 20px;
+padding: 10px 60px;
+border-radius: 5px;
+margin: 10px 0px;
+cursor: pointer;
+visibility = "hidden";
+font-family : var(--font-family);
+`;
+
+export default class Home extends Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+            activeNow: 'home',
+            image: ''
+        }
+    }
+
+    setActiveNow(activeStatus){
+        this.setState({activeNow: activeStatus});
+    }
+
+    render(){
+        return(
+            <>
+                <div className="gradient__bg">
+                    <Header/>
+                    <Feature/>
+                </div>
+                <div>
+                    <Card setState={state => this.setState(state)}/> 
+                    {this.state.activeNow === 'knowFood' && 
+                        <div>
+                            <nav className="buttontemp">
+                                <Button onClick = {() => this.setActiveNow('book') } >
+                                    Know Your Food!
+                                </Button> 
+                            </nav> 
+                        </div>
+                    }
+                    {this.state.activeNow === 'book' && <Result {...this.state}/>}
+                </div>
+                <Footer/>
+            </>
+        )
+    }
+}
--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import './Navbar.css';
-import './Navbar.css';
 import Logo from '../images/logo.png'
+import { BrowserRouter as Router,Routes, Route} from 'react-router-dom';
+import Home from './Home';
+import AboutUs from './AboutUs';
 
 const Navbar = () => {
     return (
-        <div className="g__navbar">
-      <div className="g__navbar-links">
-        <div className="g__navbar-links_logo">
-          <a href="#home"><img src={Logo} alt="logo"/></a>
+      <Router>
+        <div>
+          <div className="g__navbar-links_logo">
+            <a href="/#home"><img src={Logo} alt="logo"/></a>
+          </div>
+          <div className="g__navbar-links_container">
+            <p><a href="/#home">Home</a></p>
+            <p><a href="/#hiw">How It Works</a></p>
+            <p><a href="/#start">Try Now</a></p>
+            <p><a href="/about">About the Team</a></p>
+            <p><a href="/contact">Contact Us</a></p> 
+          </div>
         </div>
-        <div className="g__navbar-links_container">
-          <p><a href="#home">Home</a></p>
-          <p><a href="#hiw">How it works</a></p>
-          <p><a href="#start">Try Now</a></p>
-          <p><a href="#att">About the Team</a></p>
-          <p><a href="#contact">Contact Us</a></p>
-        </div>
-      </div>
-        </div>
+        <Routes>
+            <Route exact path='/' element={< Home />}></Route>
+            <Route exact path='/about' element={< AboutUs />}></Route>
+        </Routes>
+      </Router>
     )
 }
 export default Navbar;
