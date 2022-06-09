794dd688eb655ace8079616d6891c2726227d8a9
Viraj Shah <virajshah1104@gmail.com>
2022-06-08 19:56:47 -0700

    Give up on maps

--- client/src/component/Restaurants.js
+++ client/src/component/Restaurants.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import Article from './Article';
 import '../css/restaurants.css';
-import Maps from './Maps';
 
 export class Restaurants extends Component{
 
@@ -20,10 +19,6 @@ export class Restaurants extends Component{
       <div id="blog">
         <div className="g__blog-heading">
           <h1>Restaurants near you : </h1>
-          {
-            restaurants !== null && 
-              <Maps restaurants={restaurants} {...this.state}/>
-          }
         <div className="g__blog-container">
         {
         restaurants !== null && 
